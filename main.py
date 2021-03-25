d223d56cc97e9841aa4eee1af92d84732cab88c5
sguo1123 <62405530+sguo1123@users.noreply.github.com>
2021-03-25 10:11:35 -0400

    GUI Update + Progress Bar

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ Author:      HugTed
 Date:        03/20/2021
 """
 from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
 from tkinter.scrolledtext import ScrolledText
 from discord_webhook import DiscordWebhook
+import shutil
+import threading
 import time
 import requests
 import json
 import os
+import sys
 import configparser
+import math
 
 class MyWindow:
     def __init__(self, win):
+        self.s = ttk.Style()
+        self.s.theme_use('default')
+        self.s.configure("blue.Horizontal.TProgressbar", foreground='cornflower blue', background='cornflower blue')
+        self.s.configure('W.TButton', font = ('calibri', 10, 'bold', 'underline'), foreground = 'green')
+
+        self.searching = False
         self.lbl1=Label(win, text='Add ID:')
         self.t1=Entry(bd=3)
         self.btn1 = Button(win, text='Add')
@@ -26,32 +38,39 @@ class MyWindow:
         self.lbl7.place(x=25, y=50)
 
         self.lbl2=Label(win, text='Options:')
-        self.lbl2.place(x=300, y=25)
+        self.lbl2.place(x=350, y=25)
 
         self.lbl3=Label(win, text='Max Level:')
-        self.lbl3.place(x=300, y=50)
-        self.t2=Entry(bd=3)
-        self.t2.place(x=375, y=50)
+        self.lbl3.place(x=350, y=50)
+        self.t2=Entry(bd=3, width=30)
+        self.t2.place(x=425, y=50)
 
         self.lbl4=Label(win, text='Min Level:')
-        self.lbl4.place(x=300, y=75)
-        self.t3=Entry(bd=3)
-        self.t3.place(x=375, y=75)
+        self.lbl4.place(x=350, y=75)
+        self.t3=Entry(bd=3, width=30)
+        self.t3.place(x=425, y=75)
 
         self.lbl5=Label(win, text='Min Gold:')
-        self.lbl5.place(x=300, y=100)
-        self.t4=Entry(bd=3)
-        self.t4.place(x=375, y=100)
+        self.lbl5.place(x=350, y=100)
+        self.t4=Entry(bd=3, width=30)
+        self.t4.place(x=425, y=100)
 
         self.safe_mode = IntVar()
         self.is_dead = IntVar()
         self.verbose = IntVar()
         self.box1=Checkbutton(text="Remove Safe Mode", variable=self.safe_mode)
-        self.box1.place(x=375, y=125)
+        self.box1.place(x=425, y=125)
         self.box2=Checkbutton(text="Remove Dead", variable=self.is_dead)
-        self.box2.place(x=375, y=150)
+        self.box2.place(x=425, y=150)
         self.box2=Checkbutton(text="Verbose", variable=self.verbose)
-        self.box2.place(x=375, y=175)
+        self.box2.place(x=425, y=175)
+        self.b3=ttk.Button(win, style = 'W.TButton', text='Search', width=37, command=lambda:self.start_submit_thread(None, win))
+        self.b3.place(x=350, y=210)
+
+
+        self.img = ImageTk.PhotoImage(Image.open(f'{dir_path}\\smmo.png'))
+        self.image =Label(window, image = self.img, height=140, width=140)
+        self.image.place(x=415, y=245)
 
         self.lbl6=Label(win, text='Output:')
         self.lbl6.place(x=25, y=375)
@@ -64,9 +83,6 @@ class MyWindow:
         self.out1 = ScrolledText(win, height=20)
         self.out1.place(x=25, y=400)
 
-        self.b3=Button(win, text='Search', command=self.search)
-        self.b3.place(x=300, y=200)
-
         self.frame = Frame(win)
         self.frame.place(x=25, y=75)
 
@@ -78,6 +94,8 @@ class MyWindow:
         self.scrollbar.pack(side="right", fill="y")
 
         self.listNodes.config(yscrollcommand=self.scrollbar.set)
+        self.progressbar = ttk.Progressbar(win, style="blue.Horizontal.TProgressbar", length=658, mode='indeterminate')
+        self.progressbar.place(x=25, y=725)
 
         with open(f'{dir_path}\\data\\guildlist.txt', 'r') as f:
             guildlist = json.load(f)
@@ -132,60 +150,77 @@ class MyWindow:
         self.out1.delete('1.0',END)
 
     def search(self):
-        guild_name=str((self.listNodes.get(ACTIVE)))
-        guild_id = 0
-        self.out1.delete('1.0',END)
-        if guild_name == "":
-            self.out1.insert(END, "ERROR")
-            return
-        with open(f'{dir_path}\\data\\guildlist.txt', 'r') as f:
-            guildlist = json.load(f)
-        for id, name in guildlist.items():
-            if name == guild_name:
-                guild_id = id
-        if guild_id == 0:
-            self.out1.insert(END, "ERROR")
-            return
-        self.out1.insert(END, f'Found Guild - {guild_id}\n')
-        users = []
-        endpoint = f'https://api.simple-mmo.com/v1/guilds/members/{guild_id}'
-        payload = {'api_key': api_key}
-        r = requests.post(url = endpoint, data = payload)
-        lib = r.json()
-        for user in lib:
-            users.append(user["user_id"])
-        with open(f'{dir_path}\\data\\{guild_id}.txt', 'w') as f:
-            json.dump(users, f)
-        self.out1.insert(END, f'Found Members - {len(users)}\n')
-        try:
-            max_level = int(self.t2.get())
-        except:
-            max_level = 1000000
-        try:
-            min_level = int(self.t3.get())
-        except:
-            min_level = 0
-        try:
-            min_gold = int(self.t4.get())
-        except:
-            min_gold = 0
-        search_term = f'Search Params:\nMax Level: {max_level}\nMin Level: {min_level}\nMin Gold: {min_gold}\n'
-        if self.safe_mode.get() == 1:
-            search_term += "Safe Mode Players Removed\n"
-        if self.is_dead.get() == 1:
-            search_term += "Dead Players Removed\n"
-        if self.verbose.get() == 1:
-            search_term += "Verbose On\n"
-        search_term += "--------------\n"
-        self.out1.insert(END, search_term)
-        self.searchUsers(users, max_level, min_level, min_gold)
-        self.out1.insert(END, "Complete!\n")
+        while self.searching == True:
+            guild_name=str((self.listNodes.get(ACTIVE)))
+            guild_id = 0
+            self.out1.delete('1.0',END)
+            if guild_name == "":
+                self.out1.insert(END, "ERROR")
+                return
+            with open(f'{dir_path}\\data\\guildlist.txt', 'r') as f:
+                guildlist = json.load(f)
+            for id, name in guildlist.items():
+                if name == guild_name:
+                    guild_id = id
+            if guild_id == 0:
+                self.out1.insert(END, "ERROR")
+                return
+            self.out1.insert(END, f'Found Guild - {guild_id}\n')
+            users = []
+            endpoint = f'https://api.simple-mmo.com/v1/guilds/members/{guild_id}'
+            payload = {'api_key': api_key}
+            r = requests.post(url = endpoint, data = payload)
+            lib = r.json()
+            for user in lib:
+                users.append(user["user_id"])
+            with open(f'{dir_path}\\data\\{guild_id}.txt', 'w') as f:
+                json.dump(users, f)
+            self.out1.insert(END, f'Found Members - {len(users)}\n')
+            try:
+                max_level = int(self.t2.get())
+            except:
+                max_level = 1000000
+            try:
+                min_level = int(self.t3.get())
+            except:
+                min_level = 0
+            try:
+                min_gold = int(self.t4.get())
+            except:
+                min_gold = 0
+            search_term = f'Search Params:\nMax Level: {max_level}\nMin Level: {min_level}\nMin Gold: {min_gold}\n'
+            if self.safe_mode.get() == 1:
+                search_term += "Safe Mode Players Removed\n"
+            if self.is_dead.get() == 1:
+                search_term += "Dead Players Removed\n"
+            if self.verbose.get() == 1:
+                search_term += "Verbose On\n"
+            search_term += "--------------\n"
+            self.out1.insert(END, search_term)
+            self.searchUsers(users, max_level, min_level, min_gold)
+            self.out1.insert(END, "Complete!\n")
+            self.searching = False
 
     def printUser(self, lib):
         if self.verbose.get() == 0:
             self.out1.insert(END, f'[{lib["name"]}](<https://web.simple-mmo.com/user/attack/{lib["id"]}>)\n')
         else:
             self.out1.insert(END, f'Name: {lib["name"]}\nLevel: {lib["level"]}\nHP: {lib["hp"]}/{lib["max_hp"]}\nGold: {lib["gold"]}\n[Attack Link](<https://web.simple-mmo.com/user/attack/{lib["id"]}>)\n\n')
+
+    def start_submit_thread(self, event, win):
+        global submit_thread
+        submit_thread = threading.Thread(target=self.search)
+        self.searching = True
+        submit_thread.daemon = True
+        submit_thread.start()
+        self.progressbar.start()
+        win.after(1000, self.check_submit_thread(win))
+
+    def check_submit_thread(self, win):
+        if submit_thread.is_alive():
+            win.after(1000, lambda:self.check_submit_thread(win))
+        else:
+            self.progressbar.stop()
 
     def searchUsers(self, users, max_level, min_level, min_gold):
         index = 0
@@ -217,14 +252,17 @@ class MyWindow:
                     self.out1.insert(END, f'You might be rate limited, please try again later!')
             time.sleep(1)
 
+sys.setrecursionlimit(5000)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
 config.read(f'{dir_path}\\config.ini')
 api_key = config.get('DEFAULT', 'api_key')
 web_hook = config.get('DEFAULT', 'web_hook')
+version = config.get('DEFAULT', 'version_number')
 
 window=Tk()
 mywin=MyWindow(window)
-window.title('SMMO Player Checker BETA v0.3')
+window.title(f'SMMO Player Checker BETA v{version}')
+window.iconbitmap(rf'{dir_path}\\smmo.ico')
 window.geometry("700x750")
 window.mainloop()
